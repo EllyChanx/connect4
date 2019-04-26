5a5b944d11308ea3752ebc975096c09bca0bdfae
elly <elly.chan.mda@gmail.com>
2019-04-26 10:21:01 +0100

    update #selectColumn, take column numbers and players, display player on selected position with test

--- src/Connect4.js
+++ src/Connect4.js
@@ -12,7 +12,13 @@ class ConnectFour {
     }
 
     selectColumn(columnNumber, player) {
-        // implement
+        for(let i = 5; i > 0; i--) {
+            if(this.board[i][columnNumber-1] === ' ') {
+                this.board[i][columnNumber-1] = player;
+                return this.board
+            }
+        }
+        return this.board
     }
 }
 
