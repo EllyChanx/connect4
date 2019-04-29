1e92c2958855bcf271cbc1885cd29720aef7e00d
elly <elly.chan.mda@gmail.com>
2019-04-29 16:16:48 +0100

    alter spacing; use #beforeEach on test; no logic altered

--- src/Connect4.js
+++ src/Connect4.js
@@ -1,74 +1,74 @@
 class ConnectFour {
-    constructor() {
-        this.inProgress = true;
-        this.board = [
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ']
-        ];
-        this.winner;
-    }
+  constructor() {
+    this.inProgress = true;
+    this.board = [
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ']
+    ];
+    this.winner;
+  }
 
-    selectColumn(columnNumber, player) {
-        for(let i = 5; i > 0; i--) {
-            if(this.board[i][columnNumber-1] === ' ') {
-                this.board[i][columnNumber-1] = player;
-                break;
-            }
-        }
-        this.checkWinnerHorizontally(player)
-        this.checkWinnerVertically(player)
-        this.checkWinnerDiagonally(player)
-        return this.board
+  selectColumn(columnNumber, player) {
+    for(let i = 5; i > 0; i--) {
+      if(this.board[i][columnNumber-1] === ' ') {
+        this.board[i][columnNumber-1] = player;
+        break;
+      }
     }
+    this.checkWinnerHorizontally(player)
+    this.checkWinnerVertically(player)
+    this.checkWinnerDiagonally(player)
+    return this.board
+  }
 
-    checkWinnerHorizontally(player){
-        for(let i = 0; i <= 5; i++ ) {
-            let row = this.board[i].join("")
-            if (row.includes(`${player}`.repeat(4))) {
-                this.inProgress = false
-                this.winner = player
-            } else {
-                this.winner = "no winner"
-            }
-        }
-        return this.winner 
+  checkWinnerHorizontally(player){
+    for(let i = 0; i <= 5; i++ ) {
+      let row = this.board[i].join("")
+      if (row.includes(`${player}`.repeat(4))) {
+        this.inProgress = false
+        this.winner = player
+      } else {
+        this.winner = "no winner"
+      }
     }
+    return this.winner 
+  }
 
-    checkWinnerVertically(player){
-        let column = [];
-        for (let j = 0; j <= 5; j ++) {
-            for(let i = 0; i <= 5; i++){
-                column.push(this.board[i][j])
-            }
-            if (column.join("").includes(`${player}`.repeat(4))){
-                this.inProgress = false
-                this.winner = player
-            } else {
-                this.winner = "no winner"
-            }
-        }
-        return this.winner
+  checkWinnerVertically(player){
+    let column = [];
+    for (let j = 0; j <= 5; j ++) {
+      for(let i = 0; i <= 5; i++){
+        column.push(this.board[i][j])
+      }
+      if (column.join("").includes(`${player}`.repeat(4))){
+        this.inProgress = false
+        this.winner = player
+      } else {
+        this.winner = "no winner"
+      }
     }
+    return this.winner
+  }
 
-    checkWinnerDiagonally(player){
-        let diagonal = []
-        for(let i = 3; i <= 5; i++){
-            for(let j = 0; j <= 2; j++) {
-                for (let row = j, col = i; row <= i; row++, col--){
-                    diagonal.push(this.board[row][col])
-                    }
-                if (diagonal.join("").includes(`${player}`.repeat(4))){
-                    this.inProgress = false
-                    this.winner = player
-                }
-            }
+  checkWinnerDiagonally(player){
+    let diagonal = []
+    for(let i = 3; i <= 5; i++){
+      for(let j = 0; j <= 2; j++) {
+        for (let row = j, col = i; row <= i; row++, col--){
+          diagonal.push(this.board[row][col])
+          }
+        if (diagonal.join("").includes(`${player}`.repeat(4))){
+          this.inProgress = false
+          this.winner = player
         }
-        return this.winner
+      }
     }
+    return this.winner
+  }
 }
 
 module.exports = ConnectFour;
--- test/Connect4.test.js
+++ test/Connect4.test.js
@@ -1,151 +1,148 @@
 const Connect4 = require('../src/Connect4.js');
 
 describe('Connect4', () => {
-    test('starts with an empty board', () => {
-        const connect4 = new Connect4();
 
-        expect(connect4.board).toEqual([
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ']
-        ]);
-    });
+  let connect4;
 
-    test('#selectColumn take params and display on board', () => {
-        const connect4 = new Connect4();
-        expect(connect4.selectColumn(1, 1)).toEqual([
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [1, ' ', ' ', ' ', ' ', ' ']
-        ]);
+  beforeEach(function() {
+    connect4 = new Connect4();
+  });
 
-        expect(connect4.selectColumn(1, 2)).toEqual([
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [2, ' ', ' ', ' ', ' ', ' '],
-            [1, ' ', ' ', ' ', ' ', ' ']
-        ]);
+  test('starts with an empty board', () => {
+    console.log(connect4)
+    expect(connect4.board).toEqual([
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ']
+    ]);
+  });
 
-        expect(connect4.selectColumn(1, 1)).toEqual([
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [1, ' ', ' ', ' ', ' ', ' '],
-            [2, ' ', ' ', ' ', ' ', ' '],
-            [1, ' ', ' ', ' ', ' ', ' ']
-        ]);
+  test('#selectColumn take params and display on board', () => {
+    expect(connect4.selectColumn(1, 1)).toEqual([
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [1, ' ', ' ', ' ', ' ', ' ']
+    ]);
 
-        expect(connect4.selectColumn(3, 2)).toEqual([
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [1, ' ', ' ', ' ', ' ', ' '],
-            [2, ' ', ' ', ' ', ' ', ' '],
-            [1, ' ', 2, ' ', ' ', ' ']
-        ]);
-    });
+    expect(connect4.selectColumn(1, 2)).toEqual([
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [2, ' ', ' ', ' ', ' ', ' '],
+      [1, ' ', ' ', ' ', ' ', ' ']
+    ]);
 
-    test('#checkWinnerHorizontally check the board and find winner', () => {
-        const connect4 = new Connect4();
-        connect4.selectColumn(1,1);
-        connect4.selectColumn(2,1);
-        connect4.selectColumn(3,1);
-        connect4.selectColumn(4,1);
-        expect(connect4.inProgress).toEqual(false);
-        expect(connect4.checkWinnerHorizontally(1)).toEqual(1);
-    });
+    expect(connect4.selectColumn(1, 1)).toEqual([
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [1, ' ', ' ', ' ', ' ', ' '],
+      [2, ' ', ' ', ' ', ' ', ' '],
+      [1, ' ', ' ', ' ', ' ', ' ']
+    ]);
 
-    test('#checkWinnerHorizontally return no winner if dot not connected', () => {
-        const connect4 = new Connect4();
-        connect4.selectColumn(1,1);
-        connect4.selectColumn(2,1);
-        connect4.selectColumn(4,1);
-        connect4.selectColumn(5,1);
-        expect(connect4.inProgress).toEqual(true);
-        expect(connect4.checkWinnerHorizontally(1)).toEqual("no winner");
-    });
+    expect(connect4.selectColumn(3, 2)).toEqual([
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [1, ' ', ' ', ' ', ' ', ' '],
+      [2, ' ', ' ', ' ', ' ', ' '],
+      [1, ' ', 2, ' ', ' ', ' ']
+    ]);
+  });
 
-    test('#checkWinnerHorizontally return the winner', () => {
-        const connect4 = new Connect4();
-        connect4.selectColumn(1,2);
-        connect4.selectColumn(2,2);
-        connect4.selectColumn(3,2);
-        connect4.selectColumn(4,2);
-        expect(connect4.inProgress).toEqual(false);
-        expect(connect4.checkWinnerHorizontally(2)).toEqual(2);
-    });
+  test('#checkWinnerHorizontally check the board and find winner', () => {
+    connect4.selectColumn(1,1);
+    connect4.selectColumn(2,1);
+    connect4.selectColumn(3,1);
+    connect4.selectColumn(4,1);
+    expect(connect4.inProgress).toEqual(false);
+    expect(connect4.checkWinnerHorizontally(1)).toEqual(1);
+  });
 
-    test('#checkWinnerVertically win at col 1 return the winner', () => {
-        const connect4 = new Connect4();
-        connect4.selectColumn(1,1);
-        connect4.selectColumn(1,1);
-        connect4.selectColumn(1,1);
-        connect4.selectColumn(1,1);
-        expect(connect4.inProgress).toEqual(false);
-        expect(connect4.checkWinnerVertically(1)).toEqual(1);
-    });
+  test('#checkWinnerHorizontally return no winner if dot not connected', () => {
+    connect4.selectColumn(1,1);
+    connect4.selectColumn(2,1);
+    connect4.selectColumn(4,1);
+    connect4.selectColumn(5,1);
+    expect(connect4.inProgress).toEqual(true);
+    expect(connect4.checkWinnerHorizontally(1)).toEqual("no winner");
+  });
 
-    test('#checkWinnerVertically win at col 2 return the winner', () => {
-        const connect4 = new Connect4();
-        connect4.selectColumn(3,1);
-        connect4.selectColumn(3,1);
-        connect4.selectColumn(3,1);
-        connect4.selectColumn(3,1);
-        expect(connect4.inProgress).toEqual(false);
-        expect(connect4.checkWinnerVertically(1)).toEqual(1); 
-    });
+  test('#checkWinnerHorizontally return the winner', () => {
+    connect4.selectColumn(1,2);
+    connect4.selectColumn(2,2);
+    connect4.selectColumn(3,2);
+    connect4.selectColumn(4,2);
+    expect(connect4.inProgress).toEqual(false);
+    expect(connect4.checkWinnerHorizontally(2)).toEqual(2);
+  });
 
-    test('#checkWinnerDiagonally win + linear curve on row-0 col-3', () => {
-        const connect4 = new Connect4();
-        connect4.board = [
-            [' ', ' ', ' ', 1, ' ', ' '],
-            [' ', ' ', 1, ' ', ' ', ' '],
-            [' ', 1, ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [1, ' ', ' ', ' ', ' ', ' '],
-            [1, ' ', ' ', ' ', ' ', ' ']
-        ];
-        connect4.selectColumn(1,1);
-        expect(connect4.inProgress).toEqual(false);
-        expect(connect4.checkWinnerDiagonally(1)).toEqual(1);
-    });
+  test('#checkWinnerVertically win at col 1 return the winner', () => {
+    connect4.selectColumn(1,1);
+    connect4.selectColumn(1,1);
+    connect4.selectColumn(1,1);
+    connect4.selectColumn(1,1);
+    expect(connect4.inProgress).toEqual(false);
+    expect(connect4.checkWinnerVertically(1)).toEqual(1);
+  });
 
-    test('#checkWinnerDiagonally win + linear curve on row-0 col-5', () => {
-        const connect4 = new Connect4();
-        connect4.board = [
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', 1, ' ', ' '],
-            [' ', ' ', 1, ' ', ' ', ' '],
-            [' ', 1, ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ']
-        ];
-        connect4.selectColumn(1,1);
-        expect(connect4.inProgress).toEqual(false);
-        expect(connect4.checkWinnerDiagonally(1)).toEqual(1);
-    });
+  test('#checkWinnerVertically win at col 2 return the winner', () => {
+    connect4.selectColumn(3,1);
+    connect4.selectColumn(3,1);
+    connect4.selectColumn(3,1);
+    connect4.selectColumn(3,1);
+    expect(connect4.inProgress).toEqual(false);
+    expect(connect4.checkWinnerVertically(1)).toEqual(1); 
+  });
 
-    test('#checkWinnerDiagonally win + linear curve on row-2 col-5', () => {
-        const connect4 = new Connect4();
-        connect4.board = [
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', 1],
-            [' ', ' ', ' ', ' ', 1, ' '],
-            [' ', ' ', ' ', 1, ' ', ' '],
-            [' ', ' ', ' ', ' ', ' ', ' ']
-        ];
-        connect4.selectColumn(3,1);
-        expect(connect4.inProgress).toEqual(false);
-        expect(connect4.checkWinnerDiagonally(1)).toEqual(1);
-    });
+  test('#checkWinnerDiagonally win + linear curve on row-0 col-3', () => {
+    connect4.board = [
+      [' ', ' ', ' ', 1, ' ', ' '],
+      [' ', ' ', 1, ' ', ' ', ' '],
+      [' ', 1, ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [1, ' ', ' ', ' ', ' ', ' '],
+      [1, ' ', ' ', ' ', ' ', ' ']
+    ];
+    connect4.selectColumn(1,1);
+    expect(connect4.inProgress).toEqual(false);
+    expect(connect4.checkWinnerDiagonally(1)).toEqual(1);
+  });
+
+  test('#checkWinnerDiagonally win + linear curve on row-0 col-5', () => {
+    connect4.board = [
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', 1, ' ', ' '],
+      [' ', ' ', 1, ' ', ' ', ' '],
+      [' ', 1, ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ']
+    ];
+    connect4.selectColumn(1,1);
+    expect(connect4.inProgress).toEqual(false);
+    expect(connect4.checkWinnerDiagonally(1)).toEqual(1);
+  });
+
+  test('#checkWinnerDiagonally win + linear curve on row-2 col-5', () => {
+    connect4.board = [
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', 1],
+      [' ', ' ', ' ', ' ', 1, ' '],
+      [' ', ' ', ' ', 1, ' ', ' '],
+      [' ', ' ', ' ', ' ', ' ', ' ']
+    ];
+    connect4.selectColumn(3,1);
+    expect(connect4.inProgress).toEqual(false);
+    expect(connect4.checkWinnerDiagonally(1)).toEqual(1);
+  });
 
 });
